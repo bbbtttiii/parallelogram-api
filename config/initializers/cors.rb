4ef5c4050b293642f4f6829aa7ea56c0d773539b
bbbtttiii <ben.iburg@gmail.com>
2020-12-16 11:36:00 -0600

    wildcard origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000',
-    'https://bbbtttiii.github.io/parallelogram',
-    'https://www.beniburg.com/parallelogram',
-    'https://parallelogram.herokuapp.com',
-    'https://parallelogram-api.herokuapp.com'
+    origins '*'
+    
+    # 'http://localhost:3000',
+    # 'https://bbbtttiii.github.io/parallelogram',
+    # 'https://www.beniburg.com/parallelogram',
+    # 'https://parallelogram.herokuapp.com',
+    # 'https://parallelogram-api.herokuapp.com'
 
     resource '*',
       headers: :any,
