2aa271fdbda42faf5c4ab3e4169ef9c91a6a5797
bbbtttiii <ben.iburg@gmail.com>
2021-01-26 17:44:31 -0600

    remove slash from cors url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://parallelogram-mag.netlify.app/'
+    origins 'https://parallelogram-mag.netlify.app'
 
     resource '*',
       headers: :any,
