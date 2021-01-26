16f8e96e1b6f5bebb47a63a271c22c91f026812c
bbbtttiii <ben.iburg@gmail.com>
2021-01-26 17:56:59 -0600

    reset db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
 User.create(username: "Bob", email:"bob@bob.bob", password: "password")
 User.create(username: "Joe", email:"joe@joe.joe", password: "password")
 
